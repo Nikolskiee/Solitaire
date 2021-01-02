778ee5cdd17b5b35f57c2bb7d9c786c0bc5c0d8d
Federico Coscolluela <jpcoscolluela@up.edu.ph>
2021-01-02 11:31:03 +0800

    Added Menu Scene

--- Solitaire/src/application/Main.java
+++ Solitaire/src/application/Main.java
@@ -1,8 +1,10 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
 
@@ -11,8 +13,11 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,500,500);
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource("view/View.fxml"));
+			AnchorPane menu = (AnchorPane) loader.load();
+
+			Scene scene = new Scene(menu);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
