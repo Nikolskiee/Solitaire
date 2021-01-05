2c532a22a4aba9822c5efecf53bd5d476f7771a2
Nichol <nicholjohnfamadico@gmail.com>
2021-01-05 19:28:04 +0800

    Initiial Design

--- Solitaire/src/application/controller/GameOneController.java
+++ Solitaire/src/application/controller/GameOneController.java
@@ -11,13 +11,31 @@ public class GameOneController
 	
 	@FXML
 	AnchorPane pile;
+	@FXML
+	AnchorPane waste;
+	
+	@FXML
+	AnchorPane foundationHearts;
+	
+	@FXML
+	AnchorPane foundationSpades;
+	
+	@FXML
+	AnchorPane foundationDiamonds;
+	
+	@FXML
+	AnchorPane foundationClubs;
 	
 	public void setMain(Main main)
 	{
 		this.main = main;
 		System.out.println("Welcome! " + Main.player);
 		System.out.println("Mode: 1 Waste");
-		pile.setStyle("-fx-background-image: url('/2C.jpg'); -fx-background-size: 100%");
+		pile.setStyle("-fx-background-image: url('/blue.jpg'); -fx-background-size: 100%; -fx-opacity: 1");
+		foundationHearts.setStyle("-fx-background-image: url('/AH.jpg'); -fx-background-size: 100%; -fx-opacity: 0.25");
+		foundationSpades.setStyle("-fx-background-image: url('/AS.jpg'); -fx-background-size: 100%; -fx-opacity: 0.25");
+		foundationDiamonds.setStyle("-fx-background-image: url('/AD.jpg'); -fx-background-size: 100%; -fx-opacity: 0.25");
+		foundationClubs.setStyle("-fx-background-image: url('/AC.jpg'); -fx-background-size: 100%; -fx-opacity: 0.25");
 
 	}
 	
